69be3a48780e0c430be08df45db1a8d1f0ca948f
Bradyn Poulsen <bradyn@bradynpoulsen.com>
2015-01-21 15:48:27 -0700

    fix testing and ignore compiled python

--- test_sort.py
+++ test_sort.py
@@ -1,21 +1,26 @@
-import math
 import random
 import sort
 
 def generateList(n):
     a = []
     for i in range(n):
-        a.append(math.floor(random.random() * n))
+        a.append(int(random.random() * n))
     return a
 
 def test_bubble():
     arr = generateList(20)
-    assert cmp(sort.bubbleSort(arr[:]), arr.sort())
+    acp = sort.bubbleSort(arr[:])
+    arr.sort()
+    assert acp == arr
 
 def test_shaker():
     arr = generateList(20)
-    assert cmp(sort.shakerSort(arr[:]), arr.sort())
+    acp = sort.shakerSort(arr[:])
+    arr.sort()
+    assert acp == arr
 
 def test_selection():
     arr = generateList(20)
-    assert cmp(sort.selectionSort(arr[:]), arr.sort())
+    acp = sort.selectionSort(arr[:])
+    arr.sort()
+    assert acp == arr
